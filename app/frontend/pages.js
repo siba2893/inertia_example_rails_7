ca6de4d330b6883a607cd1ce01abd4e11a610374
daniel.sibaja <siba2893@gmail.com>
2022-08-28 18:02:03 -0600

    Convert to React v3

--- app/frontend/pages.js
+++ app/frontend/pages.js
@@ -1,19 +1,10 @@
-import { setupLayout } from './layout'
-
-// NOTE: Optimize the SSR bundle by not splitting by page.
-const pages = import.meta.env.SSR
-  ? import.meta.globEagerDefault('./pages/**/*.vue')
-  : import.meta.glob('./pages/**/*.vue')
+const pages = import.meta.glob('./pages/**/*.jsx')
 
 export async function resolvePage (name) {
-  let page = pages[`./pages/${ name }.vue`]
+  let page = await pages[`./pages/${ name }.jsx`]()
 
   if (!page)
-    throw new Error(`Unknown page ${ name }. Is it located under Pages with a .vue extension?`)
-
-  page = import.meta.env.SSR ? page : (await page()).default
-
-  page = setupLayout(page, name)
+    throw new Error(`Unknown page ${ name }. Is it located under Pages with a .jsx extension?`)
 
   return page
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import RubyPlugin from 'vite-plugin-ruby'
+import ReactPlugin from '@vitejs/plugin-react'
 import FullReload from 'vite-plugin-full-reload'
-import VuePlugin from '@vitejs/plugin-vue'
-import WindiCSS from 'vite-plugin-windicss'
 import IconsResolver from 'unplugin-icons/resolver'
 import Components from 'unplugin-vue-components/vite'
 import Icons from 'unplugin-icons/vite'
+import dynamicImport from 'vite-plugin-dynamic-import'
 
 export default defineConfig({
   plugins: [
     FullReload(['config/routes.rb', 'app/views/**/*']),
     RubyPlugin(),
-    VuePlugin(),
-    WindiCSS({ root: process.cwd() }),
+    ReactPlugin(),
+    dynamicImport(),
     Components({
       resolvers: [
         IconsResolver(),
@@ -20,4 +21,9 @@ export default defineConfig({
     }),
     Icons({ autoInstall: true, compiler: 'vue3' }),
   ],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.join(__dirname, 'app/frontend') }
+    ],
+  },
 })
